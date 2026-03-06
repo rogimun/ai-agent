617446b15c91ae5830bf94326b4798113d095ce8
just937 <just937@gmail.com>
2026-03-06 18:09:34 +0900

    update::23

--- shared/config.py
+++ shared/config.py
@@ -1,14 +1,11 @@
 import os
-import sys
-import asyncio
 from dotenv import load_dotenv
-from pathlib import Path
 
 load_dotenv()
 
 class Settings:
+    MODEL_NAME = os.getenv("MODEL_NAME")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     MCP_SERVER_URL = "http://localhost:8000/mcp"
-    MODEL_NAME = "gpt-4o-mini"
     
 settings = Settings()
