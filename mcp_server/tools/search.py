719a01ea68712935bc79f1d2a5e0abfe72c124d5
just937 <just937@gmail.com>
2026-03-06 16:04:24 +0900

    update:search.py

--- mcp_server/tools/search.py
+++ mcp_server/tools/search.py
@@ -1,5 +1,5 @@
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from shared.config import settings
 
 embeddings = OpenAIEmbeddings(api_key=settings.OPENAI_API_KEY)
